45dba6df521c227e55f8efdf11cb7df61f34834d
GoryKCY <Lzryyds2007@163.com>
2026-07-30 02:38:53 +0800

    chore: add main entry point

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+#include "Snake.hpp"
+#include "GameMap.hpp"
+
 int main() {
+  Snake snake;
+  GameMap gameMap(snake);
   std::cout << "I'm a Snake!!!" << "\n";
   return 0;
 } 
